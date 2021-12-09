b5bb3f60d2be44ef925c7914c8bc6b8556f9b4fb
Daniel Woodruff <92489496+ClarityCreated@users.noreply.github.com>
2021-12-09 12:36:57 -0800

    Review and write-review components

--- EzGameStore/src/app/app.module.ts
+++ EzGameStore/src/app/app.module.ts
@@ -2,65 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import { ReviewComponent } from './review/review.component';
+import { WriteReviewComponent } from './write-review/write-review.component';
 
-import {ProductListComponent} from './product-list/product-list.component';
-import {ProductViewComponent} from './product-view/product-view.component';
-import {LoginComponent } from './login/login.component';
-import {ProductResolver} from './product.resolver';
-import {ProductsResolver} from './products.resolver';
-import {RouterModule, Routes} from '@angular/router';
-import { NavbarComponent } from './navbar/navbar.component';
-import { HeaderComponent } from './header/header.component';
-import { ProductFormComponent } from './product-form/product-form.component';
-import { CheckoutFormComponent } from './checkout-form/checkout-form.component';
-const routes:Routes= [
-  {
-    path: 'add-delete-product', component: ProductFormComponent
-  },
-  {
-    path: 'list', resolve: {
-      products: ProductsResolver
-    },
-    component: ProductListComponent,
-    children: [{
-      path:"view/:productId",
-      resolve: {
-        product: ProductResolver
-      },
-      component: ProductViewComponent
-    }
-    ]
-  }, 
-  {
-    path: "edit/:productId",
-    resolve: {
-      product: ProductResolver
-    }, 
-    component: ProductFormComponent
-  }
-]
+
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    ProductViewComponent,
-    ProductListComponent,
-    NavbarComponent,
-    HeaderComponent,
-    ProductFormComponent,
-    CheckoutFormComponent
-
-
+    ReviewComponent,
+    WriteReviewComponent,
+    
 
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    RouterModule.forRoot(routes)
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- EzGameStore/src/app/write-review/write-review.component.ts
+++ EzGameStore/src/app/write-review/write-review.component.ts
@@ -1,38 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from '../product.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { FormGroup, NgForm } from '@angular/forms';
-
 
 @Component({
   selector: 'app-write-review',
   templateUrl: './write-review.component.html',
-  styleUrls: ['./write-review.component.scss']
+  styleUrls: ['./write-review.component.css']
 })
 export class WriteReviewComponent implements OnInit {
 
-
-  review: any = {
-    author: '',
-    stars: '',
-    body: ''
-  }
-
-  constructor(
-    private productService: ProductService,
-    private router: Router,
-    private route: ActivatedRoute
-  ) { }
+  constructor() { }
 
   ngOnInit(): void {
-  }
-
-  handleSubmit(event: Event, reviewFormGroup: NgForm) {
-    const reviewFormModel = reviewFormGroup.value
-    this.productService.saveReview(reviewFormModel)
-    //.subscribe(response => {
-    // this.router.navigate(["/review"])
-    //});
   }
 
 }
