cd4a35d730704a6faa041264276417298b6b1fdf
Esmeralda Vera <c.esmeralda.vera@gmail.com>
2021-12-13 09:10:14 -0600

    git issues, trying to upload

--- Java-EzGameStore/src/main/java/org/example/service/UserServiceImpl.java
+++ Java-EzGameStore/src/main/java/org/example/service/UserServiceImpl.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 
+@Service
 public class UserServiceImpl implements UserService, UserDetailsService {
 
     @Autowired
