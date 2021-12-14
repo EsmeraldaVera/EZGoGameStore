ad9a9b455ac68cec3a318a5d5f2b36cdac7e1611
Esmeralda Vera <c.esmeralda.vera@gmail.com>
2021-12-13 20:15:30 -0600

    files for authenticated login

--- Java-EzGameStore/src/main/java/org/example/EzGoGameStoreApplication.java
+++ Java-EzGameStore/src/main/java/org/example/EzGoGameStoreApplication.java
@@ -17,19 +17,12 @@ public class EzGoGameStoreApplication {
 
     @PostConstruct
     public void registerUsers(){
-        User user1=new User();
-        user1.setPassword("123");
-        user1.setUsername("test");
-        List<String> authorities1=List.of("ROLE_EMPLOYEE","ROLE_TRAINER");
-        user1.setAuthorities(authorities1);
+        User user=new User();
+        user.setPassword("123");
+        user.setUsername("test");
+        List<String> authorities1=List.of("customer_account");
+        user.setAuthorities(authorities1);
 
-        User user2=new User();
-        user2.setPassword("123");
-        user2.setUsername("Emp");
-        List<String> authorities2=List.of("ROLE_EMPLOYEE");
-        user2.setAuthorities(authorities2);
-        userService.register(user1);
-        userService.register(user2);
 
     }
     public static void main(String[] args) {
