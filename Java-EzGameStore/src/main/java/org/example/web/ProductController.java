0057137c28beb53e13837ba59091489a6e248523
BradyF <brady.falcon@revature.net>
2021-12-13 18:57:46 -0600

    moving over to main

--- Java-EzGameStore/src/main/java/org/example/web/ProductController.java
+++ Java-EzGameStore/src/main/java/org/example/web/ProductController.java
@@ -1,7 +1,7 @@
 package org.example.web;
 
 
-import org.example.entity.Product;
+}import org.example.entity.Product;
 import org.example.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,28 +24,31 @@ public class ProductController {
             value = "/api/products",
             produces = {"application/json"}
     )
-     public Collection<Product> getAll() {
+    public Collection<Product> getAll() {
         Collection<Product> products = productRepository.findAll();
         return products;
     }
+
     @GetMapping(
-            value="/api/products/{productId}",
-            produces={"application/json"}
+            value = "/api/products/{productId}",
+            produces = {"application/json"}
     )
-    public ResponseEntity<?>get(@PathVariable(name="productId")int productId){
+    public ResponseEntity<?> get(@PathVariable(name = "productId") int productId) {
         Optional<Product> optionalProduct = productRepository.findById(productId);
-        if(optionalProduct.isEmpty()) {
+        if (optionalProduct.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 
         }
         return ResponseEntity.ok(optionalProduct.get());
     }
+
     @PostMapping(
-            value="/api/products",
+            value = "/api/products",
             consumes = {"application/json", "application/xml"}
     )
     public ResponseEntity<?> createProduct(@Valid @RequestBody Product product) {
         product = productRepository.save(product);
         return ResponseEntity.status(HttpStatus.CREATED).body(product);
+
     }
 }
