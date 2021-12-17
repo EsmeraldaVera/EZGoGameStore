2ec66f0c903f43b10b8ae72ee2c45946cb5b6a52
Daniel Woodruff <92489496+ClarityCreated@users.noreply.github.com>
2021-12-16 20:40:36 -0800

    Format/SCSS

--- EzGameStore/src/app/app.module.ts
+++ EzGameStore/src/app/app.module.ts
@@ -3,26 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthGuard } from './auth.guard';
 import { JwtInterceptor } from './jwt.interceptor';
 
-import {ProductListComponent} from './product-list/product-list.component';
-import {ProductViewComponent} from './product-view/product-view.component';
-import {LoginComponent } from './login/login.component';
-import {ProductResolver} from './product.resolver';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductViewComponent } from './product-view/product-view.component';
+import { LoginComponent } from './login/login.component';
+import { ProductResolver } from './product.resolver';
 import { ProductsResolver } from './products.resolver';
-import {RouterModule, Routes} from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
 import { ProductFormComponent } from './product-form/product-form.component';
 import { CheckoutFormComponent } from './checkout-form/checkout-form.component';
-const routes:Routes= [
-  {path: 'add-delete-product', component: ProductFormComponent
-  },
-  {path: 'checkout', component: CheckoutFormComponent},
-  { path: '', pathMatch: "full", redirectTo: 'login' },
-  {path: 'list', component:ProductListComponent},
+const routes: Routes = [
+  { path: 'add-delete-product', component: ProductFormComponent },
+  { path: 'checkout', component: CheckoutFormComponent },
+  { path: '', pathMatch: "full", redirectTo: 'list' },
+  { path: 'list', component: ProductListComponent },
   { path: 'login-form', component: LoginComponent },
   {
     path: 'list', resolve: {
@@ -30,19 +29,19 @@ const routes:Routes= [
     },
     component: ProductListComponent,
     children: [{
-      path:"view/:productId",
+      path: "view/:productId",
       resolve: {
         product: ProductResolver
       },
       component: ProductViewComponent
     }
     ]
-  }, 
+  },
   {
     path: "edit/:productId",
     resolve: {
       product: ProductResolver
-    }, 
+    },
     component: ProductFormComponent
   }
 ]
@@ -56,7 +55,7 @@ const routes:Routes= [
     HeaderComponent,
     ProductFormComponent,
     CheckoutFormComponent
-  
+
   ],
   imports: [
     BrowserModule,
